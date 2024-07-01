19e07aae230876fc0b84415c84b6235f79d0b8c8
Vitaliy Kozyr <vkozyr@scand.com>
2024-07-01 13:40:24 +0300

    start bot movement

--- checkers/application.js
+++ checkers/application.js
@@ -21,6 +21,8 @@ class Cell {
 // переделать вызов .move()
 // добавить функционал для кнопки start
 
+
+// сделать рефакторинг кода
 class Game {
   board;
   user;
@@ -30,20 +32,17 @@ class Game {
   whiteCheckerAmount
 
   gameState = {
-    user : 'user',
+    user: 'user',
     bot: 'bot',
   }
   currentState = this.gameState.user;
   startGame() {
     this.generateHTML()
       .then(() => {
-        console.log("Игра началась");
-        let board = this.user.move();
-        if(board){
-          this.board = board;
-          this.rules.board = board;
-        }
-
+        // console.log("Игра началась");
+        // const fields = document.getElementById("fields");
+        // fields.addEventListener("click", this.nextMove());
+        this.nextMove();
         // let check = this.rules.checkWin();
         // console.log(this.user);
         // console.log(this.board);
@@ -52,10 +51,37 @@ class Game {
         console.error("Ошибка при инициализации игры:", error);
       });
   }
+
+  nextMove(){
+    console.log("nextMove");
+    this.user.move()
+    .then((board) => {
+      // if (board) {
+      //   // this.board = board;
+      //   // this.rules.board = board;
+      //   // this.bot.rules.board = board;
+      // }
+      //console.log(game);
+      this.bot.move();
+      // .then((board) => {
+      //   if (board) {
+      //     this.board = board;
+      //     this.rules.board = board;
+      //     this.user.rules.board = board;
+      //   }
+      //   this.nextMove();
+      // })
+      // .catch((error) => {
+      //   console.error("Ошибка во время хода бота:", error);
+      // });
+      this.nextMove();
+    })
+    .catch((error) => {
+      console.error("Ошибка во время хода:", error);
+    });
+  }
+
   constructor() {}
-  // set user(user){
-  // 	this._user = user;
-  // }
 
   generateHTML() {
     return new Promise((resolve, reject) => {
@@ -159,7 +185,44 @@ class Rules {
 
     return moves;
   }
+
+  checkIfUnderAttack(checkers) {
+    return checkers.some(checker => {
+      const possibleMoves = this.getPossibleAttacks(checker);
+      return possibleMoves.length > 0;
+    });
+  }
+
+  getPossibleAttacks(checker) {
+    const [x, y] = checker.positions;
+    const directions = [
+      [1, 1], [1, -1], [-1, 1], [-1, -1]
+    ];
+    const possibleAttacks = [];
+
+    for (let [dx, dy] of directions) {
+      const newX = x + dx * 2;
+      const newY = y + dy * 2;
+      const middleX = x + dx;
+      const middleY = y + dy;
+
+      if (this.isValidPosition(newX, newY) &&
+          this.board.cells[middleX][middleY].checker &&
+          this.board.cells[middleX][middleY].checker.color !== checker.color &&
+          !this.board.cells[newX][newY].checker) {
+        possibleAttacks.push([newX, newY]);
+      }
+    }
+
+    return possibleAttacks;
+  }
+
+  isValidPosition(x, y) {
+    return x >= 0 && x < 8 && y >= 0 && y < 8;
+  }
+
 }
+
 
 class Checker {
   constructor(color, positions) {
@@ -167,7 +230,7 @@ class Checker {
     this.positions = positions;
   }
 
-  beat() {}
+  //beat() {}
 }
 
 class CommonChecker extends Checker {}
@@ -187,6 +250,7 @@ class User {
     this.whiteCheckers = whiteCheckers;
     this.rules = rules;
   }
+
   move() {
     // this.currentState = this.clickStates.noClick;
     // this.whiteCheckers.splice(5,2);
@@ -194,112 +258,99 @@ class User {
     // return new Promise((resolve, reject) => {
       
     // });
-    const fields = document.getElementById("fields");
-    let availableMove = this.rules.getAvailableMove(
-    this.whiteCheckers,
-    this.currentState
-    );
-    let availableMove2;
-    let index;
-    // console.log(availableMove);
-    this.highlight(availableMove, this.currentState);
-    fields.addEventListener("click", (event) => {
-      let target = event.target;
-      let checkType = convert(target, "index");
-      if (!checkType) return;  
+    return new Promise((resolve) => {
+      const fields = document.getElementById("fields");
+      let availableMove = this.rules.getAvailableMove(
+      this.whiteCheckers,
+      this.currentState
+      );
+      let availableMove2;
+      let index;
+      // console.log(availableMove);
+      this.highlight(availableMove, this.currentState);
+
+      const handleClick = (event) => {
+        let target = event.target;
+        let checkType = convert(target, "index");
+        if (!checkType) return;
+
+        switch (this.currentState) {
+          case this.clickStates.noClick:
+            let check = availableMove.some(
+              (one) => one[0] === checkType[0] && one[1] === checkType[1]
+            );
+            if (!check) return;
+            this.currentState = this.clickStates.firstClick;
+            index = this.whiteCheckers.find(
+              (item) =>
+                item.positions[0] === checkType[0] &&
+                item.positions[1] === checkType[1]
+            );
+            availableMove2 = this.rules.getAvailableMove(index, this.currentState);
+            this.highlight(availableMove2, this.currentState);
+            break;
+
+          case this.clickStates.firstClick:
+            let check2 = availableMove2.some(
+              (one) => one[0] === checkType[0] && one[1] === checkType[1]
+            );
+            if (!check2) return;
+
+            this.currentState = this.clickStates.secondClick;
+
+            let indexDesired = index;
+            indexDesired.positions[0] = checkType[0];
+            indexDesired.positions[1] = checkType[1];
+
+            for (let i = 0; i < this.whiteCheckers.length; i++) {
+              if (index === this.whiteCheckers[i]) {
+                this.whiteCheckers[i].positions[0] = checkType[0];
+                this.whiteCheckers[i].positions[1] = checkType[1];
+              }
+            }
+
+            let field, fieldDesired, checker;
+            let pos;
+            for (let i = 0; i < this.rules.board.cells.length; i++) {
+              for (let j = 0; j < this.rules.board.cells.length; j++) {
+                if (index === this.rules.board.cells[i][j].checker) {
+                  this.rules.board.cells[i][j].checker = false;
+                  field = document.getElementById(`field_${i}_${j}`);
+                  checker = document.getElementById(`checker_${i}_${j}`);
+                  field.removeChild(checker);
+                }
+                if (i === checkType[0] && j === checkType[1]) {
+                  this.rules.board.cells[i][j].checker = indexDesired;
+                  fieldDesired = document.getElementById(`field_${i}_${j}`);
+                  pos = [i,j];
+                }
+              }
+            }
+            checker.id = `checker_${pos[0]}_${pos[1]}`;
+            fieldDesired.appendChild(checker);
+
+            this.highlight(availableMove, this.clickStates.noClick);
+            this.currentState = this.clickStates.noClick;
+
+            // if (this.rules.checkIfUnderAttack(this.whiteCheckers)) {
+            //   console.log("Белые шашки под боем");
+            // }
+            
+            fields.removeEventListener("click", handleClick);
+            resolve(this.rules.board);
+            break;
+          default:
+            break;
+        }
+      };
       
-      switch (this.currentState) {
-        case this.clickStates.noClick:
-          let check = false;
-          for (let one of availableMove) {
-            if (one[0] === checkType[0] && one[1] === checkType[1]) {
-              check = true;
-              break;
-            }
-          }
-          if (!check) return;    
+      fields.addEventListener("click", handleClick);
 
-          this.currentState = this.clickStates.firstClick;
-          index = this.whiteCheckers.find(
-            (item) =>
-              item.positions[0] === checkType[0] &&
-              item.positions[1] === checkType[1]
-          );
-          console.log(index);
-          availableMove2 = this.rules.getAvailableMove(index, this.currentState);
-          console.log(availableMove2);
-          this.highlight(availableMove2, this.currentState);
-          break;
-        case this.clickStates.firstClick:
-          let check2 = false;
-          for (let one of availableMove2) {
-            if (one[0] === checkType[0] && one[1] === checkType[1]) {
-              check2 = true;
-              break;
-            }
-          }
-          if (!check2) return;
-    
-          this.currentState = this.clickStates.secondClick;
+    })
+  }
 
-          console.log('--------');
-          console.log(checkType);
-          console.log(index);
-          console.log('--------');
-          let indexDesired = index;
-          indexDesired.positions[0] = checkType[0];
-          indexDesired.positions[0] = checkType[1];
+  updateBoard(){
 
-          for (let i = 0; i < this.whiteCheckers.length; i++) {
-            if(index === this.whiteCheckers[i]){
-              this.whiteCheckers[i].positions[0] = checkType[0];
-              this.whiteCheckers[i].positions[1] = checkType[1];
-            }            
-          }
-          let field, fieldDesired, checker;
-          let pos = [];
-          for (let i = 0; i < this.rules.board.cells.length; i++) {
-            for (let j = 0; j < this.rules.board.cells.length; j++) {
-              if(index === this.rules.board.cells[i][j].checker){
-                this.rules.board.cells[i][j].checker = false;
-                field = document.getElementById(`field_${i}_${j}`);
-                checker = document.getElementById(`checker_${i}_${j}`);
-                field.removeChild(checker);
-              }
-              if(i === checkType[0] && j === checkType[1]){
-                this.rules.board.cells[i][j].checker = indexDesired;
-                fieldDesired = document.getElementById(`field_${i}_${j}`);
-                pos = [i,j];
-              }
-            }
-          }
-          console.log(checker);
-          checker.id = `checker_${pos[0]}_${pos[1]}`;
-          fieldDesired.appendChild(checker);
-
-          console.log(this.rules.board);
-          // изменить положение шашки у юзера
-          // изменить положение на доске (через return)
-          // изменить доску в партии и правилах
-          // изменить верстку
-
-          console.log('--------');
-
-          this.currentState = this.clickStates.noClick;
-          availableMove = this.rules.getAvailableMove(
-            this.whiteCheckers,
-            this.currentState
-          );
-          console.log(this.currentState);
-          console.log(availableMove);
-          this.highlight(availableMove, this.currentState);
-          return this.rules.board;
-          break;
-        default:
-          break;
-      }
-  
-    });
   }
 
   highlight(moves, state) {
@@ -337,12 +388,43 @@ class User {
 }
 
 class Bot {
-  constructor(blackCheckers) {
+  constructor(blackCheckers, rules) {
     this.blackCheckers = blackCheckers;
+    this.rules = rules;
   }
 
-  move(){
-    console.log('Бот походил');
+  move() {
+    const availableMoves = this.rules.getAvailableMove(
+      this.blackCheckers,
+      "noClick"
+    );
+
+    const randomMoveIndex = Math.floor(Math.random() * availableMoves.length);
+    const [checkerX, checkerY] = availableMoves[randomMoveIndex];
+
+    const checker = this.blackCheckers.find(
+      (c) => c.positions[0] === checkerX && c.positions[1] === checkerY
+    );
+
+    const moves = this.rules.getAvailableMove_FirstClick(checker, 1);
+
+    const [newX, newY] = moves[Math.floor(Math.random() * moves.length)];
+
+    checker.positions = [newX, newY];
+
+    this.rules.board.cells[checkerX][checkerY].checker = false;
+    this.rules.board.cells[newX][newY].checker = checker;
+
+    const oldField = document.getElementById(`field_${checkerX}_${checkerY}`);
+    const checkerElement = document.getElementById(
+      `checker_${checkerX}_${checkerY}`
+    );
+    oldField.removeChild(checkerElement);
+
+    const newField = document.getElementById(`field_${newX}_${newY}`);
+    checkerElement.id = `checker_${newX}_${newY}`;
+    newField.appendChild(checkerElement);
+
   }
 }
 
