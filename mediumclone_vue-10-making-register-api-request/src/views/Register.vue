9186bc5d1b8eda9bb5d036e8e72426141d0bc6e2
Vitaliy Kozyr <vkozyr@scand.com>
2024-07-18 17:24:17 +0300

    udemy vue3 lesson10

---
+++ mediumclone_vue-10-making-register-api-request/src/views/Register.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign up</h1>
+          <p class="text-xs-center">
+            <router-link :to="{name: 'login'}">
+              Need an account?
+            </router-link>
+          </p>
+          VALIDATION ERRORS
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Username"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button
+              class="btn btn-lg btn-primary pull-xs-right"
+              :disabled="isSubmitting"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'McvRegister',
+  computed: {
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$store
+        .dispatch('register', {
+          email: 'sddaaass@dad.com',
+          username: 'ssaaasdadd',
+          password: 'sass'
+        })
+        .then(result => {
+          console.log('result from register action', result)
+        })
+    }
+  }
+}
+</script>
