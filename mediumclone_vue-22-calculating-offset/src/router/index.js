4e05492d5770e1b6dfd34eaa0c124ec1589bd134
Vitaliy Kozyr <vkozyr@scand.com>
2024-07-23 11:49:36 +0300

    vue3-lesson22

---
+++ mediumclone_vue-22-calculating-offset/src/router/index.js
@@ -0,0 +1,72 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import GlobalFeed from '@/views/GlobalFeed.vue'
+import Register from '@/views/Register.vue'
+import Login from '@/views/Login.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/',
+    name: 'globalFeed',
+    component: GlobalFeed
+  },
+  {
+    path: '/feed',
+    name: 'yourFeed',
+    component: GlobalFeed
+  },
+  {
+    path: '/tags/:slug',
+    name: 'tag',
+    component: GlobalFeed
+  },
+  {
+    path: '/articles/new',
+    name: 'createArticle',
+    component: GlobalFeed
+  },
+  {
+    path: '/articles/:slug',
+    name: 'article',
+    component: GlobalFeed
+  },
+  {
+    path: '/articles/:slug/edit',
+    name: 'editArticle',
+    component: GlobalFeed
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: GlobalFeed
+  },
+  {
+    path: '/profiles/:slug',
+    name: 'userProfile',
+    component: GlobalFeed
+  },
+  {
+    path: '/profiles/:slug/favorites',
+    name: 'userProfileFavorites',
+    component: GlobalFeed
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
