4e7ef605caac3f9eecad29dff024dc6ca612c648
Vitaliy Kozyr <vkozyr@scand.com>
2024-07-24 17:26:02 +0300

    vue3 lesson27

---
+++ mediumclone_vue-27-creating-article-page/src/router/index.js
@@ -0,0 +1,75 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import GlobalFeed from '@/views/GlobalFeed.vue'
+import YourFeed from '@/views/YourFeed.vue'
+import TagFeed from '@/views/TagFeed.vue'
+import Register from '@/views/Register.vue'
+import Login from '@/views/Login.vue'
+import Article from '@/views/Article.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/',
+    name: 'globalFeed',
+    component: GlobalFeed
+  },
+  {
+    path: '/feed',
+    name: 'yourFeed',
+    component: YourFeed
+  },
+  {
+    path: '/tags/:slug',
+    name: 'tag',
+    component: TagFeed
+  },
+  {
+    path: '/articles/new',
+    name: 'createArticle',
+    component: GlobalFeed
+  },
+  {
+    path: '/articles/:slug',
+    name: 'article',
+    component: Article
+  },
+  {
+    path: '/articles/:slug/edit',
+    name: 'editArticle',
+    component: GlobalFeed
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: GlobalFeed
+  },
+  {
+    path: '/profiles/:slug',
+    name: 'userProfile',
+    component: GlobalFeed
+  },
+  {
+    path: '/profiles/:slug/favorites',
+    name: 'userProfileFavorites',
+    component: GlobalFeed
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ mediumclone_vue-27-creating-article-page/src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import auth from '@/store/modules/auth'
+import feed from '@/store/modules/feed'
+import popularTags from '@/store/modules/popularTags'
+import article from '@/store/modules/article'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    auth,
+    feed,
+    popularTags,
+    article
+  }
+})
