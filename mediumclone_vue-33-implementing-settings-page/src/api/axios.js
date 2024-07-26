3444c5fe92ee35b009d6ba4afb0bb4b0abc630e2
Vitaliy Kozyr <vkozyr@scand.com>
2024-07-26 12:55:48 +0300

    vue3 lesson33

---
+++ mediumclone_vue-33-implementing-settings-page/src/api/axios.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import {getItem} from '@/helpers/persistanceStorage'
+
+axios.defaults.baseURL = 'https://api.realworld.io/api'
+
+axios.interceptors.request.use(config => {
+  const token = getItem('accessToken')
+  const authorizisationToken = token ? `Token ${token}` : ''
+  config.headers.Authorization = authorizisationToken
+  return config
+})
+
+export default axios
