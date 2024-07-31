cf0298feb6ba6c6299292ff62c4d716117bcee87
Vitaliy Kozyr <vkozyr@scand.com>
2024-07-31 16:17:48 +0300

    vue checkers(test)

---
+++ vue-checkers/src/App.vue
@@ -0,0 +1,26 @@
+<!-- App.vue -->
+<template>
+  <div id="app">
+    <Game />
+  </div>
+</template>
+
+<script>
+import Game from './components/Game.vue';
+
+export default {
+  components: {
+    Game,
+  },
+};
+</script>
+
+<style>
+/* Основные стили приложения */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
